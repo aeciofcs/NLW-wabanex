d4f0aa55f96663414f3eb412b5a664d4a9cd568e
Aecio Silva <aeciofcs@gmail.com>
2021-06-23 10:33:17 -0300

    continuando com as aulas.

--- lib/wabanex/user.ex
+++ lib/wabanex/user.ex
@@ -2,11 +2,18 @@ defmodule Wabanex.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @fields [:emai, :password, :name]
+
   schema "users" do
     field :email, :string
     field :name, :string
     field :password, :string
 
     timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @fields)
   end
 end
